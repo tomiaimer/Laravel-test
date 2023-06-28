799d837d3c528aab77f6e0a96eea9506752060cb
tomiaimer <107679595+tomiaimer@users.noreply.github.com>
2023-06-28 16:57:25 +0700

    Add files via upload

---
+++ post.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('container')
+    <h1> Ini adalah Halaman About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore, placeat quo, doloremque dignissimos similique eligendi excepturi quis magni, officia ipsam hic repellendus dolore quam ex. Voluptatem eaque ipsum incidunt.Vel error fugit ullam at tenetur, sit ad perferendis voluptatibus debitis repellat ea soluta sequi omnis possimus non maiores? Eum libero alias magni itaque delectus laboriosam et quam ad porro.</p>
+    <script src="js/tes.js"></script>
+    @endsection
